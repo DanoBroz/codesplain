d0dc39673bfdf364aed2817f6d06f015fa1ea88f
Daniel Brož <danobroz66@gmail.com>
2023-11-19 11:34:05 +0100

    add check for showing link to the code editor

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -7,7 +7,9 @@ function renderComponent() {
         full_name: "facebook/react",
         language: "Javascript",
         description: "js library",
-        owner: "facebook",
+        owner: {
+            login: "facebook",
+        },
         name: "react",
         html_url: "https://github.com/facebook/react",
     };
@@ -43,4 +45,18 @@ test("shows a file icon with the appropriate icon", async () => {
     });
 
     expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+    const {
+        repository: { owner, full_name },
+    } = renderComponent();
+
+    await screen.findByRole("img", { name: "Javascript" });
+
+    const link = await screen.findByRole("link", {
+        name: new RegExp(owner.login),
+    });
+
+    expect(link).toHaveAttribute("href", `/repositories/${full_name}`);
 });
